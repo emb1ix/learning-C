9081da02b5edfd4a82a32a20a26b0eac88c255e2
emb1ix <oliverejsing11@gmail.com>
2026-05-19 20:32:28 +0200

    Fixed commit message on last and made sure that no newline would be written during a push.

--- source/scripts/push.c
+++ source/scripts/push.c
@@ -9,7 +9,7 @@ int main(void) {
 
     printf("Enter commit message: ");
 
-    fgets(commit_message, sizeof(commit_message), stdin); // Gets the git pushh message
+    fgets(commit_message, sizeof(commit_message), stdin); // Gets the git-push message
     commit_message[strcspn(commit_message, "\n")] = '\0'; // Removes \n from git-push command
     snprintf(command, sizeof(command), "git commit -m \"%s\"", commit_message); // Writes the actual command for use later
 
@@ -18,6 +18,7 @@ int main(void) {
     system("git add .");
     system(command);
     system("git push -u origin main");
+
 
     printf("\n\nPushed!\n");
     return 0;
