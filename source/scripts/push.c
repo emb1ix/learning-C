561510a396bc56c6c7962ec49f53aa5aa5a5e81c
emb1ix <oliverejsing11@gmail.com>
2026-05-19 20:24:15 +0200

    è

--- source/scripts/push.c
+++ source/scripts/push.c
@@ -9,10 +9,12 @@ int main(void) {
 
     printf("Enter commit message: ");
 
-    fgets(commit_message, sizeof(commit_message), stdin);
-    snprintf(command, sizeof(command), "git commit -m \"%s\"", commit_message);
-    
+    fgets(commit_message, sizeof(commit_message), stdin); // Gets the git pushh message
+    commit_message[strcspn(commit_message, "\n")] = '\0'; // Removes \n from git-push command
+    snprintf(command, sizeof(command), "git commit -m \"%s\"", commit_message); // Writes the actual command for use later
 
+
+    // Runs the git-push commands
     system("git add .");
     system(command);
     system("git push -u origin main");
